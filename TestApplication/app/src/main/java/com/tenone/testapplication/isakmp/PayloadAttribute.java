c8d7dea8aad4e65a95088d1a732b0ab03d2d318d
Will Wang <will@stagwell.tech>
2018-05-15 10:23:40 -0400

    parse attributes in config mode 1st response

--- TestApplication/app/src/main/java/com/tenone/testapplication/isakmp/PayloadAttribute.java
+++ TestApplication/app/src/main/java/com/tenone/testapplication/isakmp/PayloadAttribute.java
@@ -1,11 +1,14 @@
 package com.tenone.testapplication.isakmp;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PayloadAttribute extends PayloadBase {
     public int type;
     public int identifier;
     public byte[] attributes;
+    public List<DataAttribute> attributeList = new ArrayList<>();
 
     public PayloadAttribute(ByteBuffer buffer) {
         super(buffer);
@@ -14,8 +17,22 @@ public class PayloadAttribute extends PayloadBase {
             type = buffer.get();
             buffer.get();
             identifier = buffer.getShort();
-            attributes = new byte[payloadLength - 8];
-            buffer.get(attributes, 0, payloadLength - 8);
+
+            if (this.payloadLength - 8 > 0) {
+                attributes = new byte[payloadLength - 8];
+                buffer.get(attributes, 0, payloadLength - 8);
+                int attSize = this.payloadLength - 8;
+                while (attSize > 0) {
+                    DataAttribute dataAttribute = new DataAttribute(buffer);
+                    if (dataAttribute != null) {
+                        attributeList.add(dataAttribute);
+                        attSize -= dataAttribute.totalLength;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
         }
     }
 }
